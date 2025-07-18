dd2a19ce30f11685a954fdd04f34bbb2da062915
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-07-19 00:17:38 +0300

    Removed core and created function add_mre_exec

---
+++ cmake/AddMreExec.cmake
@@ -0,0 +1,23 @@
+set(COMMON_DIR ${PROJECT_SOURCE_DIR}/common)
+
+function(add_mre_exec TARGET_NAME)
+    set(TARGET_NAME ${ARGV0})
+    list(REMOVE_AT ARGV 0) 
+    set(SRCS ${ARGV})
+
+    if(${CMAKE_SYSTEM_NAME} STREQUAL Generic) # For Phone
+        add_executable(${TARGET_NAME} ${SRCS} "${COMMON_DIR}/gccmain.c")
+
+        target_link_options(${TARGET_NAME} PRIVATE 
+            "-T${COMMON_DIR}/scat.ld"
+        )
+
+        set_target_properties(${TARGET_NAME} PROPERTIES SUFFIX ".axf")
+    elseif(WIN32) # For MoDis
+        add_library(${TARGET_NAME} SHARED ${SRCS} "${COMMON_DIR}/dll.def")
+    else()
+        message(FATAL_ERROR "Unsupported target system: ${CMAKE_SYSTEM_NAME}")
+    endif()
+
+    #target_link_libraries(${TARGET_NAME} mreapi)
+endfunction()
